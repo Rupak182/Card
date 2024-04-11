cc05b10c26674c2653b6a28017c98ee7199006c2
Rupak182 <rupakgoura18@gmail.com>
2024-04-11 22:43:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        lato: ["Lato", "sans-serif"],
+        poppins:["Poppins","sans-serif" ]
+
+      }
+    },
+  },
+  plugins: [],
+}
